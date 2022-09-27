6e9ecc2c2b40f133e3998f0c60e439ac94a1598c
姚头 <632319345@qq.com>
2022-09-27 11:14:34 +0800

    4-pick

---
+++ type-challenges/4-pick/index.ts
@@ -0,0 +1,26 @@
+import type { Equal, Expect } from '@type-challenges/utils';
+type MyPick<T, key extends keyof T> = {
+  [k in key]: T[k];
+};
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
+  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
+  // @ts-expect-error
+  MyPick<Todo, 'title' | 'completed' | 'invalid'>
+];
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+}
+
+interface Expected2 {
+  title: string;
+  completed: boolean;
+}
